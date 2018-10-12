2927b9a424db228e2751137a7cac6d76930496ff
exoego <mogami@exoego.net>
2018-10-12 10:58:20 +0900

    Fix missing languages.

--- site/src/main/scala/scalatex/site/Highlighter.scala
+++ site/src/main/scala/scalatex/site/Highlighter.scala
@@ -13,7 +13,7 @@ trait Highlighter{ hl =>
   val languages = mutable.Set.empty[String]
   def webjars = resource/"META-INF"/'resources/'webjars
   def highlightJs = webjars/'highlightjs/"9.12.0"
-  def highlightJsSource = webjars/"highlight.js"/"9.12.0"
+  def highlightJsSource = webjars/"highlight.js"
   def style: String = "idea"
 
   case class lang(name: String){
